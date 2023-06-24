e57a711641ea776d54af140bc7b55fa7f929db94
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-06-24 20:59:03 +0200

    initial commit lunarvim configuration

---
+++ config.lua
@@ -0,0 +1,12 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+-- use jk to exit insert mode
+lvim.keys.insert_mode["jk"] = "<ESC>"
+-- curly brace bindings
+lvim.keys.insert_mode["<C-b>"] = "<Esc>a{" 
+lvim.keys.insert_mode["<C-m>"] = "<Esc>a}"
+
+
